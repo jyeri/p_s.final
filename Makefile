515bad3806edeafa481e059b5091ba9bb5fea820
Jyri Rummukainen <Jyeri@live.fi>
2022-10-18 14:56:53 +0300

    moi

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ SRCS1 = checker.c bonus.c do_funcs1.c do_funcs2.c basic_functions1.c basic_funct
 
 SRCS2 = push_swap.c bonus.c solverbig.c solversmall.c do_funcs1.c do_funcs2.c basic_functions1.c basic_functions2.c helpers1.c swap_funcs.c check_funcs.c funnystuff.c funnyswapstuff.c libftfuncs.c gnl/get_next_line.c
 
-CFLAGS = gcc -Wall -Wextra -Werror -fsanitize=address -o
+CFLAGS = gcc -Wall -Wextra -Werror -o
 
 all: $(NAME) $(NAME2)
 $(NAME):
--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: jrummuka <jrummuka@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 18:52:26 by jrummuka          #+#    #+#             */
-/*   Updated: 2022/06/03 13:40:30 by jrummuka         ###   ########.fr       */
+/*   Updated: 2022/10/18 14:55:21 by jrummuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ int	main(int argc, char **argv)
 	char		*line;
 
 	if (argc > 1)
+	{
 		init_stack(argc, argv, &stack1, &stack2);
-	while (get_next_line(0, &line) > 0)
-		solve(&stack1, &stack2, line);
-	if (!(is_sorted(&stack1)) && stack2.actualsize == 0)
-		ft_putstr("OK\n");
-	else
-		ft_putstr("KO\n");
+		while (get_next_line(0, &line) > 0)
+			solve(&stack1, &stack2, line);
+		if (!(is_sorted(&stack1)) && stack2.actualsize == 0)
+			ft_putstr("OK\n");
+		else
+			ft_putstr("KO\n");
+	}
 }
